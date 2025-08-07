5c990fed3714c3ca14e238d400271ae63671268d
lbianco11197 <luca.bianco@euroirte.it>
2025-08-07 15:47:26 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -26,3 +26,17 @@ if committente == "TIM":
             st.markdown("[Vai al Report Delivery TIM](https://avanzamento-delivery-8ffqqvktcoli9kqk48dgki.streamlit.app/)")
 else:
     st.markdown("🚧 I report per Open Fiber sono attualmente in fase di sviluppo.")
+
+#sidebar Amministratore
+with st.sidebar:
+    st.markdown("## Area Riservata")
+    admin_access = st.checkbox("Accedi come amministratore")
+
+    if admin_access:
+        admin_pwd = st.text_input("Password", type="password")
+
+        if admin_pwd == "euroirte2025":
+            st.success("Accesso autorizzato")
+            st.markdown("[Vai all'area di gestione file 📁](https://upload-euroirte-admin.streamlit.app/)")
+        elif admin_pwd != "":
+            st.error("Password errata")
