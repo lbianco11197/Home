de398fd9e83ed555c660d38590cba8e0d5a94a42
lbianco11197 <luca.bianco@euroirte.it>
2025-08-08 17:50:31 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -107,10 +107,9 @@ if committente == "TIM":
     st.markdown("### Seleziona il tipo di report")
     col1, col2 = st.columns(2)
     with col1:
-        if st.button("📈 Report Assurance"):
-            st.markdown("[Vai al Report Assurance TIM](https://avanzamento-impulsiva-v2-ds8ntkmcuqu3mtlak3ntog.streamlit.app/)")
-    with col2:
-        if st.button("📊 Report Delivery"):
-            st.markdown("[Vai al Report Delivery TIM](https://avanzamento-delivery-8ffqqvktcoli9kqk48dgki.streamlit.app/)")
+    st.link_button("📑 Vai al Report Assurance TIM", "https://avanzamento-impulsiva-v2-ds8ntkmcuq3mtlak3ntog.streamlit.app/")
+
+with col2:
+    st.link_button("📊 Vai al Report Delivery TIM", "https://avanzamento-delivery-8ffqqvktcoi9kqk48dgki.streamlit.app/")
 else:
     st.markdown("🚧 I report per Open Fiber sono attualmente in fase di sviluppo.")
