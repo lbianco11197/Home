039e54d10efdace8e50ace409a26b03cc16b5f80
lbianco11197 <luca.bianco@euroirte.it>
2026-01-07 15:57:42 +0100

    Update news link button text

--- Home.py
+++ Home.py
@@ -132,7 +132,7 @@ with col_logo:
 with col_news:
     st.markdown("<br>", unsafe_allow_html=True)
     st.link_button(
-        "📰 News Euroirte",
+        "📰 News",
         "https://news-euroirte.streamlit.app/"
     )
 
